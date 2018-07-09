591ef73b860c20148d35d596269abdda8a90a8fc
`PixsaOJ` <`sabasaba101010@gmail.com`>
2018-07-09 23:49:47 +0400

    Cleared default animation

--- src/canvas.js
+++ src/canvas.js
@@ -43,65 +43,45 @@ function distance(x1, y1, x2, y2) {
 }
 
 // Objects
-function Particle(x, y, radius, color) {
+function Ball(x, y, radius, color) {
     this.x = x
     this.y = y
     this.radius = radius
     this.color = color
-    this.radians = Math.random() * Math.PI*2
-    this.velocity = 0.05
-    this.distacneFromCenter = randomIntFromRange(50,120)
-    this.lastMouse = {x: x, y: y}
 }
 
 Object.prototype.update = function() {
-    const lastPoint = {x:this.x,y:this.y}
-    // Move points over time
-    this.radians += this.velocity
-
-    // Drag effect
-    this.lastMouse.x += (mouse.x - this.lastMouse.x)*this.velocity
-    this.lastMouse.y += (mouse.y - this.lastMouse.y)*this.velocity
-
-    // Circular monition
-    this.x = this.lastMouse.x + Math.cos(this.radians)*this.distacneFromCenter
-    this.y = this.lastMouse.y + Math.sin(this.radians)*this.distacneFromCenter
-    this.draw(lastPoint)
+    this.draw(mouse)
 }
 
-Object.prototype.draw = function(lastPoint) {
+Object.prototype.draw = function(mouse) {
     c.beginPath()
     c.strokeStyle = this.color
-    c.lineWidth = this.radius
-    c.moveTo(lastPoint.x,lastPoint.y)
-    c.lineTo(this.x, this.y)
-    c.stroke()
+	c.arc(mouse.x,mouse.y, this.radius,0,2*Math.PI)
+	c.stroke()
     c.closePath()
 }
 
 // Implementation
-let particles
+let balls
 
 function init() {
-    particles = [];
-    for (let i = 0; i < 100; i++) {
-        const radius = (Math.random() * 4) + 1
-        particles.push(new Particle(canvas.width/2, canvas.height/2, radius, randomColor(colors)));
-    }
+	c.font = '50px Georgia'
+    const radius = (Math.random() * 4) + 1
+    ball = new Ball(canvas.width/2, canvas.height/2, radius, randomColor(colors))
 }
 
 // Animation Loop
 function animate() {
     requestAnimationFrame(animate)
-    c.fillStyle = 'rgba(255,255,255,0.05)';
-    c.fillRect(0,0, canvas.width,canvas.height)
     // c.clearRect(0, 0, canvas.width, canvas.height)
-    c.fillStyle = randomColor(colors);
-    c.fillText('o', mouse.x, mouse.y)
-
-    particles.forEach((particle) => {
-        particle.update()
-    });
+    c.fillStyle = 'white'
+    c.rect(0,0,canvas.width,canvas.height)
+    c.fill()
+    c.fillStyle = randomColor(colors)
+    c.fillText('+', mouse.x, mouse.y)
+    
+    ball.update()
 }
 
 init()
